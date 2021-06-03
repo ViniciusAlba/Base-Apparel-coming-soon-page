a0c2697d044a4696f666175631f7c26d93fdcc51
ViniciusAlba <vinicius.martins.vsm@outlook.com>
2021-06-03 18:31:14 -0300

    All the JavaScript is done. I introduce a script to check if the input have a email or not.

--- main.js
+++ main.js
@@ -1,2 +1,26 @@
-//se o input estiver vazio e a pessoa clicar em submit, dispare a mensagem
-//senão, ok.
+const email = document.getElementById("email");
+const submit = document.getElementById("submit");
+const error = document.getElementById("error");
+const messageError = document.getElementById("messageError");
+
+
+//When the button is clicked and the input is empty we have some warning
+//The same happens when the button is clicked and isn't a email.
+submit.addEventListener("click", (e) => {
+
+    if (email.validity.typeMismatch || email.value == '') {
+
+        event.preventDefault();
+        error.style.opacity = "1";
+        messageError.style.opacity = "1";
+        email.style.border = "1px solid red";
+
+    } else {
+
+        error.style.opacity = "0";
+        messageError.style.opacity = "0";
+        email.style.border = "1px solid #CCC";
+
+    }
+
+});
